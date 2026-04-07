a046bb62aade357785586c8986237e49c9367451
unknown <josecunha0740@digitalcollege.internal>
2026-04-07 10:53:39 -0300

    Projeto Bikcraft Completo: Header, Hero, Bikes e Footer

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,33 @@ import Anchor from "./components/Anchor";
 
 export default function App() {
   return (
-    <>
+    <div className="min-h-screen bg-black flex flex-col">
       <Header>
-        <Logo text="bikcraft"/>
+        <Logo text="bikcraft" />
         <Navigator>
           <UList>
             <li>
-              <Anchor text="products" href="/"/>
+              <Anchor text="products" href="#bikes" />
             </li>
             <li>
-              <Anchor text="about" href="/about"/>
+              <Anchor text="about" href="#about" />
             </li>
             <li>
-              <Anchor text="contact" href="/contact"/>
+              <Anchor text="contact" href="#footer" />
             </li>
           </UList>
         </Navigator>
       </Header>
-    </>
-  );
-}
 
+      <main className="flex-grow">
+        <Hero />
+        <div id="bikes">
+          <Bikes />
+        </div>
+      </main>
+
+      <div id="footer">
+        <Footer />
+      </div>
+   </div>
+  )};
